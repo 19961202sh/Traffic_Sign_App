3e558755d6f6fcc71ee7a9c65a79eb06b4a0d638
Durangi Abeykoon <dskabeykoon@gmail.com>
2026-01-27 15:45:21 +0530

    Initial commit -project setup
     backend implementation, and deployment to streamlit

---
+++ model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+
+class BetterCNN(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(64 * 8 * 8, 128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(128, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        return self.classifier(x)
